14be5865a0ba96e90dc36ee8792372ae0438899a
Eric Dean Kauffman <eric@moonrise.net>
2024-04-30 05:38:16 -0700

    Changed copyright for Cosmos Website Solutions, Inc. ownership.

--- WebApps/Editor/Cosmos.Editor/Models/LayoutFileUploadViewModel.cs
+++ WebApps/Editor/Cosmos.Editor/Models/LayoutFileUploadViewModel.cs
@@ -1,5 +1,5 @@
-﻿// <copyright file="LayoutFileUploadViewModel.cs" company="Moonrise Software, LLC">
-// Copyright (c) Moonrise Software, LLC. All rights reserved.
+﻿// <copyright file="LayoutFileUploadViewModel.cs" company="Cosmos Website Solutions, Inc.">
+// Copyright (c) Cosmos Website Solutions, Inc.. All rights reserved.
 // Licensed under the GNU Public License, Version 3.0 (https://www.gnu.org/licenses/gpl-3.0.html)
 // See https://github.com/MoonriseSoftwareCalifornia/CosmosCMS
 // for more information concerning the license and the contributors participating to this project.
