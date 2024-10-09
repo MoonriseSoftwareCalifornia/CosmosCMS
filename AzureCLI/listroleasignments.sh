8d46eed30fe54f3af30bf8683430f5e0098b43c4
Eric Kauffman <eric@moonrise.net>
2024-10-09 09:04:54 -0500

    Moved CLI scripts out

--- AzureCLI/listroleasignments.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "What is the resource group name?"
-read resourceGroup
-echo "What is the Cosmos DB account name?"
-read cosmosAccountName
-
-az cosmosdb sql role assignment list \
-    --resource-group $resourceGroup \
-    --account-name $cosmosAccountName  \
