59ba5105ce19b83802251deeee8a0ead8cdef7c5
Eric Dean Kauffman <eric@moonrise.net>
2024-11-18 13:40:04 -0600

    Addressing data protection bug in editor

--- WebApps/Editor/Cosmos.Editor/Program.cs
+++ WebApps/Editor/Cosmos.Editor/Program.cs
@@ -148,14 +148,14 @@ builder.Services.AddCosmosIdentity<ApplicationDbContext, IdentityUser, IdentityR
     .AddDefaultTokenProviders();
 
 // Add shared data protection here
-var container = Cosmos.BlobService.ServiceCollectionExtensions.GetBlobContainerClient(builder.Configuration, defaultAzureCredential, "ekyes");
+//var container = Cosmos.BlobService.ServiceCollectionExtensions.GetBlobContainerClient(builder.Configuration, defaultAzureCredential, "ekyes");
 
-builder.Services.AddDataProtection().UseCryptographicAlgorithms(
-    new AuthenticatedEncryptorConfiguration
-    {
-        EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
-        ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
-    }).PersistKeysToAzureBlobStorage(container.GetBlobClient("keys.xml"));
+//builder.Services.AddDataProtection().UseCryptographicAlgorithms(
+//    new AuthenticatedEncryptorConfiguration
+//    {
+//        EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
+//        ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
+//    }).PersistKeysToAzureBlobStorage(container.GetBlobClient("keys.xml"));
 
 // ===========================================================
 // SUPPORTED OAuth Providers
