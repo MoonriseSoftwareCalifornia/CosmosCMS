9c718f6ab84ff5e325a3b87051702b9d9610b375
Eric Dean Kauffman <mars@moonrise.net>
2025-01-31 16:49:32 -0600

    Added API project.

---
+++ Cosmos.Api/Controllers/ArticlesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cosmos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+    }
+}
